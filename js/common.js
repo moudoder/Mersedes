3836683ee7d4c1268e738d67dc787954f8bcb116
moudoder <39887984+moudoder@users.noreply.github.com>
2021-10-31 23:59:54 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,38 @@
 $(document).ready(function () {
   new WOW().init();
+  $(".phone").mask("+7 (999) 999-9999");
+  $(".scrol-to").on("click", function (event) {
+    event.preventDefault();
+    let id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
 
+  $('.projects-slick__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    arrows: false,
+    swipe: false,
+   });
+
+
+  let services = $('.services-block-slider');
+  for (var i = services.length - 1; i >= 0; i--) {
+    console.l
+    services[i]
+  }
+  $('.btn-modal').on('click', function() {
+    $('.dark-window').addClass('dark-window-active');
+    $('.modal-window').addClass('modal-window-active');
+    return false;
+  })
+  $('.dark-window').on('click', function() {
+    $('.dark-window').removeClass('dark-window-active');
+    $('.modal-window').removeClass('modal-window-active');
+  })
   $('.services-block-slider').slick({
     infinite: false,
     slidesToShow: 1,
@@ -12,14 +44,37 @@ $(document).ready(function () {
     nextArrow: $('.arrow__next'),
    });
 
-  $('.slider').slick({
-    infinite: false,
-    slidesToShow: 1,
+  $('.team-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
     slidesToScroll: 1,
     swipe: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    prevArrow: $('.header-arrow__left'),
-    nextArrow: $('.header-arrow__next'),
+    centerMode:true,
+    variableWidth: true,
+    prevArrow: $('.team-arrow__left'),
+    nextArrow: $('.team-arrow__right'),
    });
+
+  $('.company-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: false,
+    autoplaySpeed: 500,
+   });
+  $('.reviews-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipe: true,
+    centerMode:true,
+    variableWidth: true,
+    focusOnSelect: true,
+    prevArrow: $('.reviews-arrow__left'),
+    nextArrow: $('.reviews-arrow__right'),
+   });
+
+
+  
 })
